a4b9bc9cebb3f3487ebeebffc630f1a45e3c5571
Vanessa <hpj766@my.utsa.edu>
2021-11-03 22:16:25 -0500

    Finished commands for Draw and Drawscene

---
+++ src/Commands/DrawCommand.java
@@ -0,0 +1,26 @@
+package Commands;
+
+import Canvas.Canvas;
+import Shapes.Shape;
+
+import java.util.List;
+
+public class DrawCommand implements Command {
+    Canvas canvas;
+
+    public DrawCommand(Canvas canvas) {
+        this.canvas = canvas;
+    }
+
+    @Override
+    public void execute() {
+        int selected = canvas.getShapeSelected();
+        List<Shape> shapes = canvas.getShapes();
+        if (selected > 0){
+            Shape selectedShape = shapes.get(selected + 1);
+            selectedShape.draw();
+        } else {
+            System.out.println("no shape selected.");
+        }
+    }
+}
