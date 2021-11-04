e4c8b16c9fd330e290b26d4c6324fe2a792f6832
Raquel <raquelchu2010@gmail.com>
2021-11-03 21:49:59 -0500

    Select & Color Commands

--- src/Commands/DeleteCommand.java
+++ src/Commands/DeleteCommand.java
@@ -1,8 +1,15 @@
 package Commands;
 
+import Shapes.Shape;
+
 public class DeleteCommand implements Command {
+    Shape shape;
+
+    public DeleteCommand(Shape shape) {
+        this.shape = shape;
+    }
+
     @Override
     public void execute() {
-
     }
 }
--- src/Commands/SelectCommand.java
+++ src/Commands/SelectCommand.java
@@ -1,9 +1,27 @@
 package Commands;
 
+import Canvas.Canvas;
+import Shapes.Shape;
+
+import java.util.List;
+
 public class SelectCommand implements Command {
+    Canvas canvas;
+    int selected;
+
+    public SelectCommand(Canvas canvas, int selected) {
+        this.canvas = canvas;
+        this.selected = selected;
+    }
 
     @Override
     public void execute() {
-
+        canvas.setShapeSelected(selected);
+        List<Shape> shapes = canvas.getShapes();
+        if (selected > 0 && selected <= shapes.size()){
+            canvas.setShapeSelected(selected);
+        } else {
+            System.out.println("ERROR: invalid shape form SELECT");
+        }
     }
 }
