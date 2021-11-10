79503a1c2323b90cb1e5c9e1eb2ff7cd78ccf5f7
Bethany Salazar <bethanysalazar2007@hotmail.com>
2021-11-09 20:46:03 -0600

    COMPLETE: bash script, makefile, updated main for input

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,3 +1,5 @@
+package Main;
+
 import Canvas.Canvas;
 
 import java.io.File;
@@ -5,7 +7,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         // TODO: take input file from command line (use args[0] instead of hardcoded string)
-        File inputFile = new File("InputSet1.txt");
+        File inputFile = new File(args[0]);
 
         Canvas canvas = new Canvas();
         canvas.startDrawing(inputFile);
