800bfd3195dd977fb85a47af22cd4caf411accf0
taylorkulawik <kdt668@my.utsa.edu>
2021-11-09 20:56:34 -0600

    Cleaned Code again

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,7 +6,6 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: take input file from command line (use args[0] instead of hardcoded string)
         File inputFile = new File(args[0]);
 
         Canvas canvas = new Canvas();
